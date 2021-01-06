9cad045099fd9f00dd80484b06c0c58d9ba2d4a4
Aelor <aelor.code@gmail.com>
2021-01-07 01:56:38 +0300

    update: task pug

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,11 +1,22 @@
 const {src, dest, task} = require('gulp')
 const pug = require('gulp-pug')
+const notify = require('gulp-notify')
+const fs = require('fs')
 
 module.exports = function() {
    task('pugTask', () => {
       return src(path.pug.src)
          .pipe(pug({
+            locals: {
+               main: JSON.parse(fs.readFileSync(path.json.dest + path.json.name, 'utf-8'))
+            },
             pretty: true
+         }))
+         .on('error', notify.onError(error => {
+            return {
+               title: 'Pug',
+               message: error.message
+            }
          }))
          .pipe(dest(path.pug.dest))
    })
