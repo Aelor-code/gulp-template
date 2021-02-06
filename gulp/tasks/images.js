0ff5fb56d11dcc848b58f7f986c9d8e2a4568bd7
Alor <aelor.code@gmail.com>
2021-02-06 20:58:03 +0300

    update images task

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,8 +1,21 @@
 const {src, dest, task} = require('gulp')
+const gulpif = require('gulp-if')
+const imagemin = require('gulp-imagemin')
 
 module.exports = () => {
    task('imagesTask', () => {
       return src(path.images.src)
+         .pipe(gulpif(!isDev, imagemin([
+             imagemin.gifsicle({interlaced: true}),
+             imagemin.mozjpeg({quality: 75, progressive: true}),
+             imagemin.optipng({optimizationLevel: 5}),
+             imagemin.svgo({
+                 plugins: [
+                     {removeViewBox: true},
+                     {cleanupIDs: false}
+                 ]
+             })
+         ])))
          .pipe(dest(path.images.dest))
          .pipe(server.stream())
    })
