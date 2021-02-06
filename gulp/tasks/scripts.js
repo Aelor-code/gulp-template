0576a5b5b56a57e4776f7e618a03b214a2a8d2bf
Alor <aelor.code@gmail.com>
2021-02-06 20:39:41 +0300

    update script task

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,11 +1,19 @@
 const {src, dest, task} = require('gulp')
 const concat = require('gulp-concat')
+const gulpif = require('gulp-if')
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
 
 module.exports = () => {
    task('scriptsTask', () => {
       return src(path.scripts.src)
          .pipe(concat(path.scripts.fileName))
+         .pipe(gulpif(!isDev, dest(path.scripts.dest)))
+         .pipe(rename({
+            basename: path.scripts.fileName + '.min'
+         }))
+         .pipe(gulpif(!isDev, uglify()))
          .pipe(dest(path.scripts.dest))
-         .pipe(server.stream())
+         .pipe(gulpif(isDev, server.stream()))
    })
 }
