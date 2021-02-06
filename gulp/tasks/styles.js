106cb90bfa744131550d54d4259d443b7b9c46eb
Alor <aelor.code@gmail.com>
2021-02-06 20:25:51 +0300

    update styles task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,22 +4,29 @@ const rename = require('gulp-rename')
 const autoprefixer = require('gulp-autoprefixer')
 const sourcemaps = require('gulp-sourcemaps')
 const groupMediaQueries = require('gulp-group-css-media-queries')
+const gulpif = require('gulp-if')
+const csso = require('gulp-csso')
 
 module.exports = () => {
    task('stylesTask', () => {
       return src(path.styles.src)
-         .pipe(sourcemaps.init())
+         .pipe(gulpif(isDev, sourcemaps.init()))
          .pipe(stylus())
          .pipe(autoprefixer({
             overrideBrowserslist: ['last 3 version'],
             cascade: true
          }))
          .pipe(groupMediaQueries())
-         .pipe(sourcemaps.write())
+         .pipe(gulpif(isDev, sourcemaps.write()))
+         .pipe(gulpif(!isDev, rename({
+            basename: path.styles.fileName
+         })))
+         .pipe(gulpif(!isDev, dest(path.styles.dest)))
          .pipe(rename({
             basename: path.styles.fileName + '.min'
          }))
+         .pipe(gulpif(!isDev, csso()))
          .pipe(dest(path.styles.dest))
-         .pipe(server.stream())
+         .pipe(gulpif(isDev, server.stream()))
    })
 }
