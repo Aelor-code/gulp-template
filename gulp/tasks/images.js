7e4a8f71a08680eb1a4c9def54bbe9a425030628
Alor <aelor.code@gmail.com>
2021-02-11 20:20:12 +0300

    update images task

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,10 +1,12 @@
 const {src, dest, task} = require('gulp')
 const gulpif = require('gulp-if')
 const imagemin = require('gulp-imagemin')
+const changed = require('gulp-changed')
 
 module.exports = () => {
    task('imagesTask', () => {
       return src(path.images.src)
+         .pipe(changed(path.images.dest))
          .pipe(gulpif(!isDev, imagemin([
              imagemin.gifsicle({interlaced: true}),
              imagemin.mozjpeg({quality: 75, progressive: true}),
